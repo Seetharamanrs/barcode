b7142b46d87990809c8e22ff474455fec85a0b4e
seetharamanrs <seetharaman2632001@gmail.com>
2025-06-30 19:39:15 +0100

    Updated

---
+++ barcode_utils.py
@@ -0,0 +1,15 @@
+import barcode
+from barcode.writer import ImageWriter
+from io import BytesIO
+from IPython.display import Image, display
+def barcode_gen(data:str):
+    c='0123456789QWERTYUIOPASDFGHJKLZXCVBNM-. $/+%'
+    for i in data:
+        if i not in c:
+            raise ValueError("Invalid Character for code 39. Use A-Z 0-9 and -.$/+% space ")
+    b=barcode.get_barcode('code39')   
+    b_i=b(data.upper(),writer=ImageWriter())
+    buffer = BytesIO()
+    b_i.write(buffer)
+    buffer.seek(0)
+    return buffer
